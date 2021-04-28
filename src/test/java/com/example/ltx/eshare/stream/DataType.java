73bd29c24d7e3793dae01e5532e5b54a7963fce9
Liutx <15666882775@163.com>
2021-04-28 14:20:14 +0800

    BigDecimal

--- src/test/java/com/example/ltx/eshare/stream/DataType.java
+++ src/test/java/com/example/ltx/eshare/stream/DataType.java
@@ -1,8 +1,8 @@
 package com.example.ltx.eshare.stream;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Liutx
@@ -20,18 +20,29 @@ public class DataType {
 //        System.out.println(String.format("%.1f", num1));//四舍五入
 //        System.out.println(String.format("%.1f", num2));
 
-        DecimalFormat format = new DecimalFormat("#.##");
-        //四舍五入策略   StringFormat使用的是HALF_UP
-        format.setRoundingMode(RoundingMode.DOWN);
-        System.out.println(format.format(num1));
-        format.setRoundingMode(RoundingMode.DOWN);
-        System.out.println(format.format(num2));
+//        DecimalFormat format = new DecimalFormat("#.##");
+//        //四舍五入策略   StringFormat使用的是HALF_UP
+//        format.setRoundingMode(RoundingMode.DOWN);
+//        System.out.println(format.format(num1));
+//        format.setRoundingMode(RoundingMode.DOWN);
+//        System.out.println(format.format(num2));
+//
+//        //正确示例:
+//        BigDecimal numRight1 = new BigDecimal("3.35");
+//        //setScale() 对数据小数点后多少位后进行处理
+//        BigDecimal numRight2 = numRight1.setScale(1,BigDecimal.ROUND_DOWN);
+//        System.out.println(numRight2);
+//        BigDecimal numRight3 = numRight1.setScale(1,BigDecimal.ROUND_HALF_UP);
+//        System.out.println(numRight3);
+        Set<BigDecimal> hashSet1 = new HashSet<>();
+        hashSet1.add(new BigDecimal("1.0"));
+        System.out.println(hashSet1.contains(new BigDecimal("1")));
 
-        //正确示例:
-        BigDecimal numRight1 = new BigDecimal("3.35");
-        BigDecimal numRight2 = numRight1.setScale(1,BigDecimal.ROUND_DOWN);
-        System.out.println(numRight2);
-        BigDecimal numRight3 = numRight1.setScale(1,BigDecimal.ROUND_HALF_UP);
-        System.out.println(numRight3);
+
+        Set<BigDecimal> hashSet2 = new HashSet<>();
+        hashSet2.add(new BigDecimal("1.0").stripTrailingZeros());
+        System.out.println(hashSet1.contains(new BigDecimal("1.000").stripTrailingZeros()));
+
+
     }
 }
